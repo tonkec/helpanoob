a311d87e487459c98aa1a5f689def815a0bf746f
antonija_simic <antonija1023@gmail.com>
2016-01-26 18:02:44 +0100

    add validations for desc and content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :content, presence: true
   default_scope -> { order(created_at: :desc) }
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  validates :description, presence: true
   default_scope -> { order(created_at: :desc) }
 end

9ace4bf8e8abd6e5b21fb0e34d724bdc5a947a85
antonija_simic <antonija1023@gmail.com>
2016-01-26 17:26:12 +0100

    show comments on posts page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,16 @@
 class CommentsController < ApplicationController
   def show
     @posts = current_user.posts
-
   end
+
+  def index
+    @comments = Comment.all
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
+  end
+
+  private
+    def load_parent
+      @post = Post.find(params[:post_id])
+   end
 end

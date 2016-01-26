4e9aa169a31132ff8301d7aa18860c9cf4ebb276
antonija_simic <antonija1023@gmail.com>
2016-01-26 18:12:37 +0100

    add association through

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts
 end

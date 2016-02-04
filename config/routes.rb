19273a2900c5d6bcf3e1d80bca7ba613afb2fc81
antonija_simic <antonija1023@gmail.com>
2016-02-04 13:51:40 +0100

    add route for showing user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :users, only: [:show]
   devise_for :users
   devise_scope :user do
     get "sign_in", to: "devise/sessions#new"

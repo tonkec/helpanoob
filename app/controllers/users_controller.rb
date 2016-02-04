20098cbeef51537db85300832c51c942c24f2df0
antonija_simic <antonija1023@gmail.com>
2016-02-04 13:58:19 +0100

    redirect after successful sign_in; prevent non sgined in users to access home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
 
   def show
     @post = current_user.posts.build if user_signed_in?
-    @user = User.find(params[:id])
+    
   end
    
 end

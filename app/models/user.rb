2d0d7c5eba23339686f713688dd5ddedba7a7135
antonija_simic <antonija1023@gmail.com>
2016-02-05 15:37:53 +0100

    add validations for user's email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, through: :posts
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
   mount_uploader :avatar, AvatarUploader
 end

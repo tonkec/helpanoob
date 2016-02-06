47574869b6771e87047bbf18333dcaa4de96f535
antonija_simic <antonija1023@gmail.com>
2016-02-06 15:24:16 +0100

    add user_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131200709) do
+ActiveRecord::Schema.define(version: 20160206142318) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"

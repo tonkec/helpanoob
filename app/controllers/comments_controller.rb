a9f05f26479e1ef94981e26f3838c2bd1a60c022
antonija_simic <antonija1023@gmail.com>
2016-02-07 21:50:45 +0100

    enable editing with best_in_place

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,27 +30,25 @@
 
 
   def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
   def update 
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     respond_to do |format|
-      if @comment.update(comments_params)
-        format.html { redirect_to post_path(@post), notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: post_path(@post) }
+      if @comment.update_attributes(comments_params)
+        format.html { redirect_to(@comment, :notice => 'comment was successfully updated.') }
+        format.json { respond_with_bip(@comment) }
       else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { respond_with_bip(@comment) }
       end
     end
   end
 
   private
-    def load_parent
-      @post = Post.find(params[:post_id])
-    end
-
     def comments_params
       params.require(:comment).permit(:content)
     end

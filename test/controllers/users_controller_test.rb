b587f2d3649d8bb14222ae9206fc28850217f088
antonija_simic <antonija1023@gmail.com>
2016-02-09 11:05:24 +0100

    fix warden error

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   include Devise::TestHelpers                          
   include Warden::Test::Helpers                        
-  Warden.test_mode!   
+  Warden.test_mode! 
   
   test "should get show" do
+    sign_in users(:antonija)
     get :show
     assert_response :success
   end

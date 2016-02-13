30f4eba193cf8c314a5a0388c4a158971959d0fb
antonija_simic <antonija1023@gmail.com>
2016-02-13 19:50:40 +0100

    add more tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,32 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:pero)
+    @user.password = "password"
   end
 
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
-    puts @user.errors.full_messages
+  end
+
+   test "password should be present" do
+    @user.password = "     "
+    assert_not @user.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
 end

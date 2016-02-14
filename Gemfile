9152221657084bdceaf3873164352fcbcfe2c97d
antonija_simic <antonija1023@gmail.com>
2016-02-14 19:57:44 +0100

    add sendgrid gem; remove old mailer

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.0'
 gem "font-awesome-rails"
 gem 'devise'
 gem 'best_in_place', '~> 3.0.1'
+gem 'sendgrid'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,10 @@
+class UserNotifier < ApplicationMailer
+  default :from => 'helpanoob@noreply.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+end

5f01db8c855ffaa8736459794d27d87dc5ed0060
antonija_simic <antonija1023@gmail.com>
2016-02-14 19:03:14 +0100

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def profile
     @user_posts = current_user.posts.paginate(page: params[:page])
   end
    
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  get "profile", to: "users#show"
+  get "profile", to: "users#profile"
+  get '/user/:id', to: 'users#show'
+
   devise_for :users
   devise_scope :user do
     get "sign_in", to: "devise/sessions#new"

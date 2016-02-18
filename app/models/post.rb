3ae030b136ce4dc5073420b729f569ddeca4dfc1
antonija_simic <antonija1023@gmail.com>
2016-02-18 16:57:52 +0100

    add votable; problem with order by votes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
   has_many :comments, dependent: :destroy
-
+  default_scope -> { order(cached_votes_up: :desc) }
   validates :description, presence: true
 end

9034684f4fbe195779c1ac1b98ada53e23d53763
antonija_simic <antonija1023@gmail.com>
2016-02-19 17:11:50 +0100

    add search input field

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order(:cached_votes_up => :desc)
+    @posts = Post.all
   end
 
   # GET /posts/1

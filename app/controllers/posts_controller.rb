e95416967c221e1d7de85e7546e2a6f367ac9189
antonija_simic <antonija1023@gmail.com>
2016-02-21 19:46:12 +0100

    stupid ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @post = Post.new
     @q = Post.ransack(params[:q])
     @posts_s = @q.result(distinct: true)
   end
@@ -30,12 +31,17 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = current_user.posts.build(post_params)
-    if @post.save
-       redirect_to root_path 
-       flash[:success] = 'Post was successfully created.'
+    respond_to do |format|
+     if @post.update(post_params)
+      format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      format.json { render :show, status: :ok, location: @post }
+      format.js
     else
-      render :new
+      format.html { render :new }
+      format.json { render json: @post.errors, status: :unprocessable_entity }
+      format.js
     end
+  end
   end
 
   # PATCH/PUT /posts/1
@@ -45,9 +51,11 @@ class PostsController < ApplicationController
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
+        format.js
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -59,6 +67,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 

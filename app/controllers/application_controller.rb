7d6680a8224426863d9dd47ce49a2a8ecc22f3b6
antonija_simic <antonija1023@gmail.com>
2016-03-06 13:45:19 +0100

    start implementation for multiple image uploads for posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :search
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    root_path
+    posts_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
     home_path
+  end
+
+  def after_sign_up_path_for(resource)
+    posts_path
   end
 
   def search
@@ -23,6 +27,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:avatar, :name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :avatar, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :last_name, :first_name, :email, :avatar) }
   end
 end

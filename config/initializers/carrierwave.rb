df6cc06baafd4a836ddebd11169e654050ac18f7
antonija_simic <antonija1023@gmail.com>
2016-04-16 11:16:56 +0200

    path_style true

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_S3_REGION']
+    region: ENV['AWS_S3_REGION'],
+    path_style: true
   }
   config.fog_directory = ENV['S3_BUCKET_NAME']
 end

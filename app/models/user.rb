6788464c263faba8b8cb90867de99ddf0735daf0
antonija_simic <antonija1023@gmail.com>
2016-04-17 15:07:59 +0200

    uniqe username

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :avatar, presence: true
   mount_uploader :avatar, AvatarUploader
 

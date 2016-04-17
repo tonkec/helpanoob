ef2fdd91bdfc7b506a40353d496545432c57b054
antonija_simic <antonija1023@gmail.com>
2016-04-17 13:16:42 +0200

    change order of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,7 +28,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 20, max: 200}
   validates :description, presence: true, length: {minimum: 50}
   validates :user_id, presence: true
-  default_scope -> { order(cached_votes_up: :desc) }
+  default_scope -> { order(created_at: :desc) }
 
   scope :uncommented, -> {
     where(:comments_count => 0)

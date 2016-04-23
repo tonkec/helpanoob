f41cec462f485291382d47bbbdf60c946f98fa63
antonija_simic <antonija1023@gmail.com>
2016-04-23 15:35:08 +0200

    change host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  host = 'https://hidden-thicket-61545.herokuapp.com'
+  host = 'https://helpanoob.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
  config.action_mailer.smtp_settings = {

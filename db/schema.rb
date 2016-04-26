9918a0208d37263222442c17641302a1cdce2269
antonija_simic <antonija1023@gmail.com>
2016-04-26 23:11:55 +0200

    remove old seeding data; remove notifications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416123624) do
+ActiveRecord::Schema.define(version: 20160425144050) do
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.integer  "bootsy_resource_id"
@@ -71,6 +71,17 @@ ActiveRecord::Schema.define(version: 20160416123624) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "recipient_id"
+    t.integer  "actor_id"
+    t.datetime "read_at"
+    t.string   "action"
+    t.integer  "notifiable_id"
+    t.string   "notifiable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "post_attachments", force: :cascade do |t|

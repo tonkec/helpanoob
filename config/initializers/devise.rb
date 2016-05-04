dbd65a8593df3af3df98e6ad3491f661edeaf35a
antonija_simic <antonija1023@gmail.com>
2016-05-04 15:48:17 +0200

    add secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,5 +8,6 @@ Devise.setup do |config|
   config.password_length = 8..128
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
-  config.secret_key = ENV['DEVISE_SECRET_TOKEN']
+  config.secret_key = '865768315e884b88c97bfb84c1ab09f0d634b682fe76c526792fb08c8ec871af0ffa04f4434b0a772cdb09cd749ceeb18de854c57bfc24fece157ff06423b5a4'
+
 end

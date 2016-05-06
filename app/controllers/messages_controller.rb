e379df1c8c39e3226c33b411e812a87330313470
antonija_simic <antonija1023@gmail.com>
2016-05-06 19:31:09 +0200

    showing notification for message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,5 +9,21 @@ class MessagesController < ApplicationController
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversations_path
+    subject = "subject"
+    body = "body"
+    
+    Mailboxer::Notification.notify_all(recipients,
+    subject, 
+    body,
+    obj = conversation,
+    sanitize_text = true,
+    notification_code = nil, 
+      send_mail = true)
+    #User.first.notify("Hi", "What is up")
+
+    #recipients.each do |recipient|
+     # recipient.notify("You got new message",
+      # "Let s pretend here is message body")
+    #end
   end
 end

afd8c7e37945afc1cc3320f83c8b5e17dfeea9fd
antonija_simic <antonija1023@gmail.com>
2016-05-08 18:42:36 +0200

    test messages notifications

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,14 +11,13 @@ RSpec.describe MessagesController, type: :controller do
 
   let(:user1) {FactoryGirl.create(:user, username: "toncica")}
   let!(:user2) {FactoryGirl.create(:user, username: "perica")}
-  before(:each) do
-    visit conversations_path
-    sign_in user1
-  end 
+ 
 
   describe "creating message" do
-
-    it "creates new message" do
+  
+    it "creates new message and shows notification" do
+      visit conversations_path
+      sign_in user1
       subject = "Message subject"
       body = "Message body"
       click_link "Compose Message"
@@ -27,14 +26,18 @@ RSpec.describe MessagesController, type: :controller do
       fill_in "message_body", with: body
       click_button "Send message"
       expect(page).to have_content("Message has been sent!")
-
-      click_link "Sent"
-
       expect(user1.mailbox.sentbox.first.subject).to eq(subject)
-
       expect(user2.mailbox.inbox.first.subject).to eq(subject)
+      expect(user2.mailbox.inbox).not_to be_empty
+
+      find(:xpath, "//a[@href='/users/sign_out']").click
+      
+      sign_in user2
+      expect(page).to have_content("#{user1.username} sent you a message!")
+      expect(page).to have_content(user1.username)
+
     end
-
-
   end
+
+
 end

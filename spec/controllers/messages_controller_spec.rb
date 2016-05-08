b4a7b7fd2b2afc820c973627a27fa861fe87427c
antonija_simic <antonija1023@gmail.com>
2016-05-08 17:56:28 +0200

    test sending messages

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,22 +1,40 @@
-=begin
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      save_and_open_page
-      expect(response).to have_http_status(:success)
-    end
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    user.confirm!
+    click_button "Sign in"
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+  let(:user1) {FactoryGirl.create(:user, username: "toncica")}
+  let!(:user2) {FactoryGirl.create(:user, username: "perica")}
+  before(:each) do
+    visit conversations_path
+    sign_in user1
+  end 
 
+  describe "creating message" do
+
+    it "creates new message" do
+      subject = "Message subject"
+      body = "Message body"
+      click_link "Compose Message"
+      find('#recipients_select').find(:xpath, 'option[4]').select_option 
+      fill_in "message_subject", with: subject
+      fill_in "message_body", with: body
+      click_button "Send message"
+      expect(page).to have_content("Message has been sent!")
+
+      click_link "Sent"
+
+      expect(user1.mailbox.sentbox.first.subject).to eq(subject)
+
+      expect(user2.mailbox.inbox.first.subject).to eq(subject)
+    end
+
+
+  end
 end
-=end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "Caspian Star #{n}" }
+    sequence(:username) { |n| "User-#{n}" }
     sequence(:email) { |n| "test#{n}@example.com" }    
     password "password"
     password_confirmation "password"

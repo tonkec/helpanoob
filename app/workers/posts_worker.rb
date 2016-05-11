1df139363d9062d58290a770d8d52d10ac86d8e7
antonija_simic <antonija1023@gmail.com>
2016-05-11 09:04:32 +0200

    remove puts

--- app/workers/posts_worker.rb
+++ app/workers/posts_worker.rb
@@ -5,8 +5,6 @@ class PostsWorker
   def perform(user_id)
     user = User.find_by_id(user_id)
     post = Post.first
-    puts user
-    puts post
     PostMailer.new_post_email(user_id, post).deliver
   end
 end

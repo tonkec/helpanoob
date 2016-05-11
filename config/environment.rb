4fe00b64defd3a1ba09d14b1a1206768dc20ef82
antonija_simic <antonija1023@gmail.com>
2016-05-11 09:14:57 +0200

    how to load Redis? :O

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,4 @@
 # Load the Rails application.
-Rails.application.configure do
-  config.gem 'redis'  
-end
-
 require File.expand_path('../application', __FILE__)
 # Load the app's custom environment variables here, so that they are loaded before environments/*.rb
 app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')

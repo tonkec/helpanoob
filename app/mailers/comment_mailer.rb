6c7223af36a6f9cd53a81c834d6d8a00c2db6b8c
brarnaudovski <brankoarnaudovski@yahoo.com>
2016-05-20 18:01:13 +0200

    send mail notification when someone answers on a question
    notification when someone answers to your question => user should
    receive email

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,11 @@
+class CommentMailer < ApplicationMailer
+
+  def new_answer_is_made(post_user_id, answer_id)
+    @answer = Comment.find answer_id
+    @answer_user = @answer.user
+    @post_user = User.find post_user_id
+
+    mail(to: @post_user.email, subject: 'New answer has been created!')
+  end
+
+end

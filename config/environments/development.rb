6af7e7cdea409a7868e55d8853d3fb9f73469134
brarnaudovski <brankoarnaudovski@yahoo.com>
2016-05-20 17:56:55 +0200

    use http for action mailer in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,10 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host,protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.delivery_method = :letter_opener
- 
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

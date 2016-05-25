247c51baf55d6e062bb205cad2fa4c9ac9b334db
antonija_simic <antonija1023@gmail.com>
2016-05-25 21:13:51 +0200

    new callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,5 +10,5 @@ Devise.setup do |config|
   config.sign_out_via = :delete
   config.secret_key = 'edc2c0c5a45cc5a2f9cc436649521f1744a1f295171c3577533b3ccb40cccce5e7b9e1104c2100d34d4aae768bdd2119cdb60bee3cb22816fe120309130d3e15'
   config.omniauth :github,  ENV['github_key'], ENV['github_secret'], scope: 'user,public_repo',
-                  callback_url: "http://localhost:3000"
+                  callback_url: "https://helpanoob.herokuapp.com/"
 end

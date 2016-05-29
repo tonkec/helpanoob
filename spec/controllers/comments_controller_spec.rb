4a405073c26e9d9f4111840aab252ea6a87672e9
antonija_simic <antonija1023@gmail.com>
2016-05-29 21:54:11 +0200

    test mark as read and current user should not receive notification

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -30,23 +30,44 @@ RSpec.describe CommentsController, type: :controller do
         fill_in "Say something smart...", with: "say something smart"
         expect { click_button "Comment" }.to change(Notification, :count).by(1)
 
+        fill_in "Say something smart...", with: "Another smart thing"
+        expect { click_button "Comment" }.to change(Notification, :count).by(1)
+
+        # USER WHO POSTED SHOULD NOT RECEIVE AN EMAIL
+        expect(pero.notifications.count).to eq(0)
+
         find("a[href='#{destroy_user_session_path}']").click
 
         sign_in antonija
         visit post_path(antonija_post)
-
+        #DROPDOWN CONTENT
         expect(antonija.notifications).to_not be_empty
         expect(page).to have_content("Comment on your question")
         expect(page).to have_css(".dropdown-menu li##{antonija.notifications.first.id}")
         expect(page).to have_css(".panel-body", text: pero.username)
-        expect(page).to have_selector("li##{antonija.notifications.first.id}", count: antonija.notifications.count)
+        expect(page).to have_selector("li##{antonija.notifications.first.id}")
+        expect(page).to have_selector("li##{antonija.notifications.last.id}")
 
-        #find(".dropdown-menu li##{antonija.notifications.first.id}").click
-        #save_and_open_page
-        #expect(antonija.notifications.first.read).to eq(true)
+        #MARK AS READ
+        find(".dropdown-menu li##{antonija.notifications.first.id} a").click
+        find(".dropdown-menu li##{antonija.notifications.last.id} a").click
+        expect(antonija.notifications.first.read).to eq(true)
+        expect(antonija.notifications.last.read).to eq(true)
 
       end
 
+      describe "email about new notification" do
+        it "has right content" do
+          fill_in "Say something smart...", with: "say something smartish"
+          click_button "Comment"
+
+          find("a[href='#{destroy_user_session_path}']").click
+
+          sign_in antonija
+          visit post_path(antonija_post)
+          #save_and_open_page
+        end
+      end
 
     end #dropdown
 

76be1791339a6004cfda3a7ad89b2dde7ce02e7d
antonija_simic <antonija1023@gmail.com>
2016-06-01 18:38:09 +0200

    error with inline attachments

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,6 +1,7 @@
 class CommentMailer < ApplicationMailer
 
   def new_answer_is_made(post_user_id, answer_id)
+    
     @answer = Comment.find answer_id
     @answer_user = @answer.user
     @post_user = User.find post_user_id
--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,7 @@
 class PostMailer < ApplicationMailer
 
   def new_post_email(user_id, post)
+
     @user = User.find_by_id(user_id)
     @post = post
 

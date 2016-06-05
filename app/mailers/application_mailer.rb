373483b78285f2f721e059a4ee970d79392b7344
brarnaudovski <brankoarnaudovski@yahoo.com>
2016-06-05 19:45:15 +0200

    fix: DRY for mailer attachments

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   default from: "noreply-helpanoob@helpanoob.com"
   layout 'mailer'
 
-  attachments.inline['logo.png'] = File.read('public/images/logo.png')
-  attachments.inline['twitter.png'] = File.read('public/images/twitter.png')
-  attachments.inline['facebook.png'] = File.read('public/images/facebook.png')
+  before_filter :add_inline_attachments!
+
+  private
+    def add_inline_attachments!
+      attachments.inline['logo.png'] = File.read('public/images/logo.png')
+      attachments.inline['twitter.png'] = File.read('public/images/twitter.png')
+      attachments.inline['facebook.png'] = File.read('public/images/facebook.png')
+    end
 end

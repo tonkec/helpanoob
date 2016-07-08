8241dd537cc0246259b1972e2ffde38a2c194912
antonija_simic <antonija1023@gmail.com>
2016-07-08 20:27:42 +0200

    add new var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  host = 'https://helpanoob.herokuapp.com'
+  host = ENV["HOST_APPLICATION"]
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

0018e119eb7fc15ec0fffd4fb88b1ebaa0d78d62
antonija_simic <antonija1023@gmail.com>
2016-07-08 21:18:21 +0200

    send email to all users when new q is asked

--- app/workers/posts_worker.rb
+++ app/workers/posts_worker.rb
@@ -1,9 +1,9 @@
 class PostsWorker
  include Sidekiq::Worker
 
-  def perform(user_id)
+  def perform(user_id, post_id)
     user = User.find_by_id(user_id)
-    post = Post.first
+    post = Post.find post_id
     PostMailer.new_post_email(user_id, post).deliver
   end
 end
